13578d0d9eed5ca3850964ac589b59727556ccb8
Boris Yanchev <60177356+Borkoyan@users.noreply.github.com>
2020-03-30 22:48:06 +0100

    Move H-bridge libraries

---
+++ Libraries/L298/L298.cpp
@@ -0,0 +1,37 @@
+/*
+  L298.h - Test library for L298 H-bridge.
+  Written by Boris Yanchev.
+
+*/
+
+#include "L298.h"
+
+actuator::actuator(int in1, int in2, int en)
+{
+  in1Pin = in1;
+  pinMode(in1Pin, OUTPUT);
+
+  in2Pin = in2;
+  pinMode(in2Pin, OUTPUT);
+
+  enPin = en;
+  pinMode(enPin, OUTPUT);
+
+}
+
+void actuator::extending()
+{
+  digitalWrite(in1Pin, HIGH);
+  digitalWrite(in2Pin, LOW);
+}
+
+void actuator::retracting()
+{
+  digitalWrite(in1Pin, LOW);
+  digitalWrite(in2Pin, HIGH);
+}
+
+
+void actuator::changeForce(int percentage) {
+  analogWrite(enPin, percentage / 100 * 255);
+}
